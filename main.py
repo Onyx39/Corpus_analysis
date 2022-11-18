c304f0c45ebf80e5f38fe820723e66f43edd7a07
Valentin <valentin.richard39@sfr.fr>
2022-11-18 16:49:53 +0100

    modification main.py

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from src.print_structure import main
 from src.print_month_data import main2
 from src.occurance_counting import create_list_occurancies, graph
-import os
+from src.word_searching import print_all, create_day_legend
+from src.word_collecting import word_collecting
+import random as rd
+import time as t
+
+start = t.time()
 
 #main()
 #main2()
 #create_list_occurancies()
-graph(create_list_occurancies())
+#graph(create_list_occurancies())
+#test_create_list_occurancies()
+#explore_data()
+#print_all('france')
+#print_all("mali")
+#create_day_legend()
+l = word_collecting()
+word = l[rd.randint(0, len(l))]
+print_all(word)
+
+end = t.time()
+print("\nThe time of execution of above program is :", round(100*(end-start))/100, "s\n")
